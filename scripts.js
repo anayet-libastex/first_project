60695bfb076eb9d7c8bf0f2734cd68a2424adaf4
Anayet Libastex <anayethossain030@gmail.com>
2024-05-25 09:15:18 +0600

    Professional Image Gallery using  html css & Javascript

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal-img");
+    const captionText = document.getElementById("caption");
+    const closeBtn = document.getElementsByClassName("close")[0];
+
+    document.querySelectorAll(".gallery-image").forEach(image => {
+        image.addEventListener("click", function() {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        });
+    });
+
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
